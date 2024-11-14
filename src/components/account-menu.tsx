5fb724bc3d4bfeaa85c7133051724f48a3817e3a
Izaelle <izaelleavrillavigne@gmail.com>
2024-11-14 14:52:27 -0300

    feat integrating api into the dashboard profile

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,8 +8,19 @@ import {
 import { Button } from "./ui/button";
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
 
 export function AccountMenu() {
+  const {data: profile} = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const {data: managedRestaurant} = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getProfile,
+  })
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -17,15 +28,15 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Izaelle Lavigne</span>
+          <span>{profile?.name}</span>
           <span className="text-xs font-normal text-muted-foreground">
-            izaelleavrillavigne@gmail.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
